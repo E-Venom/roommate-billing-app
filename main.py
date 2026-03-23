55ed873d56b47bbafe40a4c896102ea87d7e34b2
Johnny Bryant <mbryant@mines.edu>
2026-03-22 18:54:57 -0600

    Added CLI (Command Line Interface) for user to communicate with the app.

--- main.py
+++ main.py
@@ -97,17 +97,26 @@ class PDFReport:
 
         webbrowser.open(self.filename)
 
+# CLI - COMMAND LINE (USER) INTERFACE IMPLEMENTATION
+bill_amount = float(input("Please enter the bill's amount: "))
+period = input("Please is the billing period? E.g. March 2021: ")
+name_roommate1 = input("Please enter the name of roommate1: ")
+name_roommate2 = input("Please enter the name of roommate2: ")
+roommate1_days_in_house = int(input(f"Please enter the number of days {name_roommate1} stayed in the house for {period}: "))
+roommate2_days_in_house = int(input(f"Please enter the number of days {name_roommate2} stayed in the house for {period}: "))
+
+
 # Testing what is implemented so far
-the_bill = Bill(amount=120, period="March 2022")
-roommate1 = Roommate(name="John", days_in_house=20)
-roommate2 = Roommate(name="Mary", days_in_house=25)
-roommate1_payment = roommate1.pays(bill=the_bill, other_roommate=roommate2)
-roommate2_payment = roommate2.pays(bill=the_bill, other_roommate=roommate1)
+the_bill = Bill(bill_amount, period)
+roommate1 = Roommate(name_roommate1, roommate1_days_in_house)
+roommate2 = Roommate(name_roommate2, roommate2_days_in_house)
+roommate1_payment = roommate1.pays(the_bill, roommate2)
+roommate2_payment = roommate2.pays(the_bill, roommate1)
 
 assert round(roommate1_payment + roommate2_payment) == the_bill.amount
 
 print(f'{roommate1.name} pays: ${roommate1_payment:.2f}')
 print(f'{roommate2.name} pays: ${roommate2_payment:.2f}')
 
-pdf_report = PDFReport(filename="Report1.pdf")
-pdf_report.generate_pdf(roommate1=roommate1, roommate2=roommate2, bill=the_bill)
+pdf_report = PDFReport(filename=f"Report {the_bill.period}.pdf")
+pdf_report.generate_pdf(roommate1, roommate2, the_bill)
